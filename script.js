566a832972fdb2555b99057ea9dd939581a4ab79
Evan Talavera <S10206687@connect.np.edu.sg>
2021-01-10 04:19:54 +0800

    Initial API linking

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    var $Character = $("#Character");
+
+    $.ajax({
+        type: "GET",
+        url: "https://api.genshin.dev/characters/albedo",
+        success: function(data) {
+            $.each(Character, function(i, test){
+                $Character.append("<li>"+ test.name + test.description + "</li>" )
+            })
+            
+        }
+    })
+})
